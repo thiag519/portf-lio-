d9c83bcd01de5f379723fcf91bd18f6d61fe3ccd
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-15 17:27:16 -0300

    style- criando slider

--- script.ts
+++ script.ts
@@ -3,3 +3,16 @@
 // Botão de menu mobile
 // Animação do nome
 // Animação das habilidades slider
+    //
+    let numberImg:number ;
+    let stringO ;
+    stringO = document.querySelector('title');
+    numberImg = document.querySelectorAll('.skills').length;
+
+    function testar() {
+        alert(numberImg)
+    }
+    testar();
+    
+    
+   
