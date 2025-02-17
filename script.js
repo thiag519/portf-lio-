b5b5b7c1312750e97e515a6e13092509f7086b36
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-17 10:59:10 -0300

    remove- retirei script.ts e coloquei script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// Botão de menu inicial
+
+// Botão de menu mobile
+// Animação do nome
+// Animação das habilidades slider
+    
+        let numberImg ;
+        let stringO ;
+        stringO = document.querySelector('title');
+        numberImg = document.querySelectorAll('.skill').length;
+
+        function testar() {
+           
+            console.log(numberImg)
+        }
+           
+
+    
+   
