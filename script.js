b0c339abcc63a1623bc86e7113a3028e5c9db096
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-18 20:01:51 -0300

    style- criando menu mobile

--- script.js
+++ script.js
@@ -1,23 +1,21 @@
 // Botão de menu inicial
-
 // Botão de menu mobile
+    let menuMob = document.querySelector('.menu-mobile');
+
+   /* function menuToggle() {
+        if( menuMob.style.display = 'none' ) {
+             menuMob.style.display = 'flex';
+        }else{
+            menuMob.style.display = 'none';
+        }
+       
+    }*/
 // Animação do nome
 // Animação das habilidades slider
     
-    let numberImg ;
-    let stringO ;
-    let start = 0;
-        stringO = document.querySelector('title').innerText;
-        numberImg = document.querySelectorAll('.skill').length;
 
-    function testar() {
-        start ++;
-        if(start > (numberImg -1)) {
-            start = 0
-        }
-        
-    }
-    setInterval(testar, 3000);
+
+    
            
 
     
