4e8b4d2195fbfbc289c849e7669d2391719e30ab
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-19 19:43:43 -0300

    style- continuando edicao do menu

--- script.js
+++ script.js
@@ -2,14 +2,13 @@
 // Botão de menu mobile
     let menuMob = document.querySelector('.menu-mobile');
 
-   /* function menuToggle() {
-        if( menuMob.style.display = 'none' ) {
-             menuMob.style.display = 'flex';
-        }else{
-            menuMob.style.display = 'none';
-        }
-       
-    }*/
+    function menuToggle() {
+         if(menuMob.style.display = 'none') {
+            menuMob.style.display = 'flex';
+         } 
+            !menuMob.style.display ;
+               
+    }
 // Animação do nome
 // Animação das habilidades slider
     
