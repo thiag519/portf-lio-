a23209be8dad24f33496b8e9b6cc24979b522fc2
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-20 18:47:42 -0300

    style- funacao de abrir e fechar o menu

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 // Botão de menu inicial
 // Botão de menu mobile
     let menuMob = document.querySelector('.menu-mobile');
+    let mmLines = document.querySelectorAll('.mm-line');
+   
 
     function menuToggle() {
-         if(menuMob.style.display = 'none') {
-            menuMob.style.display = 'flex';
-         } 
-            !menuMob.style.display ;
-               
+      menuMob.style.display === 'flex'? 
+      menuMob.style.display = 'none':
+      menuMob.style.display = 'flex';
+      mmLines[0].classList.toggle('animationUp')
+      mmLines[2].classList.toggle('animationLow')
+      mmLines[1].classList.toggle('animationMiddle')        
     }
 // Animação do nome
 // Animação das habilidades slider
