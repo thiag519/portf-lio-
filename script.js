2af044b9ba9a79b7f0e9c96d0f9febca3283b630
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-22 20:14:02 -0300

    style- and fix- melhorando menu fixad

--- script.js
+++ script.js
@@ -4,14 +4,27 @@
    
 
     function menuToggle() {
-      menuMob.style.display === 'flex'? 
-      menuMob.style.display = 'none':
+    if(menuMob.style.display === 'flex') {
+      menuMob.style.display = 'none';
+    }else{
       menuMob.style.display = 'flex';
-      mmLines[0].classList.toggle('animationUp')
-      mmLines[2].classList.toggle('animationLow')
-      mmLines[1].classList.toggle('animationMiddle')        
+    }
+      mmLines[0].classList.toggle('animationUp');
+      mmLines[2].classList.toggle('animationLow');
+      mmLines[1].classList.toggle('animationMiddle')  ;      
     }
 
+    function closeMenu() {
+      
+      setTimeout(() => {
+        
+        menuMob.style.display = 'none'
+      }, 50);
+      mmLines[0].classList.toggle('animationUp');
+      mmLines[2].classList.toggle('animationLow');
+      mmLines[1].classList.toggle('animationMiddle');
+    }
+    
     
 
 
