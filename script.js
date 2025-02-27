f7859a7fd91d9bf028e64949f0e06c0904bb6e1d
thiago souza <thiagosouzadasilva78@gmail.com>
2025-02-27 20:34:25 -0300

    feat- criacao de active no menu mobile

--- script.js
+++ script.js
@@ -5,13 +5,17 @@
   // abrir e feichar o menu
     function menuToggle() {
     if(menuMob.style.display === 'flex') {
-      menuMob.style.display = 'none';
+      menuMob.style.opacity= '1';
+      setTimeout(() => {
+        menuMob.style.opacity= '0';
+        menuMob.style.display = 'none';
+      }, 500);
     }else{
       menuMob.style.opacity= '0';
       setTimeout( () =>{
         menuMob.style.display = 'flex';
         menuMob.style.opacity= '1';
-      }, 300)
+      }, 500)
       
     }
       mmLines[0].classList.toggle('animationUp');
@@ -29,6 +33,29 @@
       mmLines[2].classList.toggle('animationLow');
       mmLines[1].classList.toggle('animationMiddle');
     }
+    // ativar area de clique do menu
+    let menuBtn = document.querySelectorAll('.menu li');
+    
+    menuBtn.forEach((item) => {
+      item.addEventListener('click', () => {
+        menuBtn.forEach((item) => {
+          item.classList.remove('active');
+        });
+        item.classList.add('active');
+      });
+    });
+    // ativar area de clique do menu mobile
+    
+    let menuBtnMob = document.querySelectorAll('.menu-mobile li'); 
+
+    menuBtnMob.forEach((item) => {
+      item.addEventListener('click', () => {
+        menuBtnMob.forEach((item) => {
+          item.classList.remove('active');
+        });
+        item.classList.add('active');
+      });
+    });
     
     
 
