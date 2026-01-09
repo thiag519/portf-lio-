7fe16b3cee591d7bf1947bd7f82e643a131b57a9
thiago souza <thiagosouzadasilva78@gmail.com>
2026-01-09 15:20:46 -0300

    style/ editei imagens ajustei fonts

--- projetos.js
+++ projetos.js
@@ -1,32 +1,40 @@
 const projetos = [
   { id:1, 
     titulo:'Salão de Beleza',
-    imagem: 'projetos/Império-da-Beleza.PNG', 
-    descricao: ["Desafio: Criar um site institucional para um salão de beleza que transmitisse profissionalismo e facilitasse o contato com clientes.", "Solução: Desenvolvimento de uma aplicação web moderna com React, Next.js, TypeScript e Tailwind CSS, focada em responsividade, usabilidade e organização do código.", "Resultado: O projeto resultou em uma presença digital mais profissional para o salão. Até o momento, o site já conta com dois agendamentos realizados."] ,
+    imagem: 'projetos/Império-da-Beleza.png', 
+    descricao: ["Desafio: Criar um site institucional para um salão de beleza que transmitisse profissionalismo e facilitasse o contato com clientes.", "Solução: Desenvolvi uma aplicação web moderna com React, Next.js, TypeScript e Tailwind CSS, focada em responsividade, usabilidade, organização do código e tecnicas de SEO como otimização de imagens e meta tags.", "Resultado: O projeto resultou em uma presença digital mais profissional para o salão. Até o momento, o site já conta com dois agendamentos realizados."] ,
     linkgit: 'https://github.com/thiag519/salao-de-beleza-copia.git',
     linkdeploy: 'https://www.imperiodabelezaposse.com.br/'
   },
    { id:2, 
     titulo:'Envio de Email',
-    imagem: 'projetos/Area-de-envio.PNG', 
-    descricao: ["Desafio: Criar um servidor com Node.js para envio de email e conectar com o front-end do meu Portfólio." ,"Solução: Desenvolver API em Node.js com Express, CORS, Resend(API de e-mail) e TypeScript para enviar mensagens do formulário do meu portfólio diretamente para meu e-mail, hospedagem Serverless na Vercel.","Resultado: Por em pratica conhecimento em Node e conexão Back-end, Front-end e API."] ,
+    imagem: 'projetos/Area-de-envio.png', 
+    descricao: ["Desafio: Criar um servidor com Node.js para envio de email e conectar com o front-end do meu Portfólio." ,"Solução: Desenvolvi uma API em Node.js com Express, CORS, Resend(API de e-mail) e TypeScript para enviar mensagens do formulário do meu portfólio diretamente para meu e-mail, hospedagem Serverless na Vercel.","Resultado: Por em pratica conhecimento em Node e conexão Back-end, Front-end e API."] ,
     linkgit: 'https://github.com/thiag519/send-.git',
     linkdeploy: 'https://thiag519.github.io/portf-lio-/#footer-contact'
   },
-   { id:3,
+  { id:3,
+    titulo:'Calculadora IMC',
+    imagem:'projetos/IMC.png',
+    descricao:['Desafio: Criar calculadora de IMC onde o usuário preenche seu peso e altura e recebe uma informaçao sobre como esta seu IMC.','Solução: Desenvolvi a calculadora IMC usando componentização com React, CSS e TypeScript.','Resultado: Uma calculadora de IMC eficiente e com resultado imediato.'],
+    linkgit:'https://github.com/thiag519/react-calc-imc.git',
+    linkdeploy:'https://react-calc-imc-delta.vercel.app/'
+  },
+   { id:4,
     titulo:'Pagina Starbucks',
-    imagem:'projetos/Pagina-Starbucks.jpeg',
-    descricao:['Desafio: Criar cópia do site da Starbucks para por em prática conhecimento em Grids.', 'Solução: Utilizar CSS com Grids para criar interface responsiva e moderna', 'Resultado: Consolidei a capacidade de desenvolver sites responsivos e bem estruturados usando CSS Grid.'],
+    imagem:'projetos/Pagina-Starbucks.png',
+    descricao:['Desafio: Criar cópia do site da Starbucks para por em prática conhecimento em Grids.', 'Solução: Utilizei CSS com Grids para criar interface responsiva e moderna.', 'Resultado: Consolidei a capacidade de desenvolver sites responsivos e bem estruturados usando CSS Grid.'],
     linkgit:'https://github.com/thiag519/c-pia-starbucks.git',
     linkdeploy:'https://thiag519.github.io/c-pia-starbucks/'
   },
-   { id:4,
+   { id:5,
     titulo:'Pizzaria',
-    imagem:'projetos/Pizzas.PNG',
-    descricao:['Desafio: Desenvolver um catálogo de pizzas interativo que possibilita escolher o sabor, tamanho da pizza, adicione ao carrinho e saber o total do seu pedido na hora', 'Solução: Utilizei HTML para a estrutura, CSS para o styling e layout responsivo, e JavaScript para a lógica de negócio e interatividade.', 'Resultado: Etapa de desenvolvimento da loja de pizzas concluída, demonstrando domínio em HTML, CSS e JavaScript, com foco em responsividade e usabilidade.'],
+    imagem:'projetos/Pizzas.png',
+    descricao:['Desafio: Desenvolver um catálogo de pizzas interativo que possibilita escolher o sabor, tamanho da pizza, adicione ao carrinho e saber o total do seu pedido na hora.', 'Solução: Utilizei HTML para a estrutura, CSS para o styling e layout responsivo, e JavaScript para a lógica de negócio e interatividade.', 'Resultado: Etapa de desenvolvimento da loja de pizzas concluída, demonstrando domínio em HTML, CSS e JavaScript, com foco em responsividade e usabilidade.'],
     linkgit:'https://github.com/thiag519/projeto_pizza.git',
     linkdeploy:'https://thiag519.github.io/projeto_pizza/'
   }
+  
 ]
   /*{ id:4,
     titulo:'Pagina Web',
@@ -57,11 +65,5 @@ const projetos = [
     linkgit:'https://github.com/thiag519/quiz-javascript.git',
     linkdeploy:'https://thiag519.github.io/relogio-analogico/'
   },
-   { id:7,
-    titulo:'Calculadora IMC',
-    imagem:'projetos/IMC.PNG',
-    descricao:['Calculadora de IMC onde o usuário preenche seu peso e altura e recebe uma informaçao sobre como esta seu IMC','Interatividade desenvolvida com React, CSS e TypeScript','Componentização utilizando React e TypeScript'],
-    linkgit:'https://github.com/thiag519/react-calc-imc.git',
-    linkdeploy:'https://react-calc-imc-delta.vercel.app/'
-  }
+   
   */
